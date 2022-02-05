8bc6982feedb36d41e066ef2df4390ba3945127a
Khaled <khaledmourad23345@gmail.com>
2022-02-05 19:57:57 +0200

    some changes in settings.py file

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(04j8gmh$g57%=wkuuzl%ilf!08gnqs7r1-r+ra(qq2sxs-d(9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
